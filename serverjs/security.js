7e1f8a37e45cd00b2192ddad5cd0bb26e6e2c61d
maaguaete <tigermaaguaete@gmail.com>
2022-09-16 21:33:26 +0700

    make it better

---
+++ serverjs/security.js
@@ -0,0 +1,38 @@
+let pass;
+const bcrypt = require('bcrypt');
+
+function encrypt(text) {
+    let base64 = require('base64-js');
+    return base64.toByteArray(text).toString();
+}
+
+function decrypt(bytes) {
+    let base64 = require('base64-js');
+    return base64.fromByteArray(bytes).toString();
+}
+
+function en_bcrypt(text) {
+    let encoded_text = "";
+
+    bcrypt.hash(text, 10)
+        .then(hash => {
+
+            text = hash;
+            console.log(pass);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+    return encoded_text;
+}
+
+function compare(text, encryptText) {
+    bcrypt.compare(text, encryptText)
+        .then(result => { return result })
+        .catch(err => { err })
+
+}
+en_bcrypt("Toan@1108");
+
+module.exports = { encrypt, decrypt, en_bcrypt, compare };
